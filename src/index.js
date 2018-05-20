23ebc53b364c6e4152ccbec57250143fa9c2e93d
Rupesh <rupesx26@gmail.com>
2018-05-21 00:05:30 +0530

    new pro

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+// setTimeout(() => {
+//    ReactDOM.unmountComponentAtNode(document.getElementById('root'));}, 10000);
+registerServiceWorker();
